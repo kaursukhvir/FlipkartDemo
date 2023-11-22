1a931b3d4305200f226b3065eb6c021662fdbdef
= <=>
2023-11-22 15:27:23 +0530

    added demo java project

---
+++ FlipkartJavaDemo/src/com/flipkart/flipkartjavademo/Demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.flipkartjavademo;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
